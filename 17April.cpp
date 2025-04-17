cffb1d2c2646e8a763c74ddb1ff9560da8cb9da6
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-17 13:20:06 +0530

    check Anagram

--- 17April.cpp
+++ 17April.cpp
@@ -1,25 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// bool isSubsRec(string s1, string s2, int n, int m){
+//     if(m==0) return true;
+//     if(n==0) return false;
+//     if(s1[n-1] == s2[m-1]){
+//         return isSubsRec(s1, s2, n-1, m-1);
+//     }else
+//     return isSubsRec(s1, s2, n-1, m);
+// }
 int main()
 {
-    // check if a string is a subsequence of other
-    string str1 = "ABCD";
-    string str2 = "AD";
-    int m = str1.length();
-    int n = str2.length();
-    int i = 0, j = 0;
-    while (i < m && j < n)
+    string str1 = "listen";
+    string str2 = "silen";
+    vector<int> count(26, 0);
+    for (int i = 0; i < str1.length(); i++)
     {
-        if (str1[i] == str2[j])
-        {
-            i++;
-            j++;
-        }
-        i++;
+        count[str1[i] - 'a']++;
+        count[str2[i] - 'a']--;
     }
-    bool flag = (j == n);
-    cout << flag << "\n";
+    for (int i = 0; i < 26; i++)
+    {
+        if (count[i] > 0)
+        {
+            cout << "not anagram\n";
+        }
+    }
+    cout << "program executed successfully\n";
+
+    // check if a string is a subsequence of other
+    // string str1 = "ABCD";
+    // string str2 = "AD";
+    // int m = str1.length();
+    // int n = str2.length();
+    // int i = 0, j = 0;
+    // while (i < m && j < n)
+    // {
+    //     if (str1[i] == str2[j])
+    //     {
+    //         i++;
+    //         j++;
+    //     }
+    //     i++;
+    // }
+    // bool flag = (j == n);
+    // cout << flag << "\n";
     // return ;
 
     // palindrome check
