d1a918423cf7bbe7387b9e3682f535c2155230a2
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-17 12:58:08 +0530

    palindrome check

--- 17April.cpp
+++ 17April.cpp
@@ -3,21 +3,38 @@ using namespace std;
 
 int main()
 {
-    // print frequencies of given string
-    string str = "kuldeep";
-    vector<int> count(26, 0);
-    for (int i = 0; i < str.length(); i++)
-    {
-        count[str[i] - 'a']++;
-    }
+    // palindrome check
+    string str = "ABCDBA";
+    // string curr = str;
+    // reverse(str.begin(), str.end());
 
-    for (int i = 0; i < 26; i++)
+    // bool flag = (str == curr);
+    // cout << flag << "\n";
+
+    int low = 0, high = str.length() - 1;
+    while (low <= high)
     {
-        if (count[i] > 0)
-        {
-            cout << (char)(i + 'a') << " " << count[i] << "\n";
-        }
+        swap(str[low], str[high]);
+        low++;
+        high--;
     }
+    cout << str << "\n";
+
+    // print frequencies of given string
+    // string str = "kuldeep";
+    // vector<int> count(26, 0);
+    // for (int i = 0; i < str.length(); i++)
+    // {
+    //     count[str[i] - 'a']++;
+    // }
+
+    // for (int i = 0; i < 26; i++)
+    // {
+    //     if (count[i] > 0)
+    //     {
+    //         cout << (char)(i + 'a') << " " << count[i] << "\n";
+    //     }
+    // }
 
     return 0;
 }
