f055ff17771fcc59756bc241c674dd458777cb4e
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-28 04:56:48 +0530

    Array Leaders(GFG)

--- 28April.cpp
+++ 28April.cpp
@@ -89,3 +89,21 @@ public:
         return ans;
     }
 };
+
+// Array Leaders
+vector<int> leaders(vector<int> &arr)
+{
+    // Code here
+    int n = arr.size();
+    vector<int> leaders;
+    leaders.push_back(arr[n - 1]);
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] >= leaders.back())
+        {
+            leaders.push_back(arr[i]);
+        }
+    }
+    reverse(leaders.begin(), leaders.end());
+    return leaders;
+}
