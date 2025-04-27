efad52328c4a102df9e8a379eff21a1d515cd989
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-27 10:30:58 +0530

    approch 2 for get maximum product

--- 27April.cpp
+++ 27April.cpp
@@ -121,3 +121,26 @@ public:
     }
 };
 // approach 2
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+        int n = nums.size();
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] < 0)
+            {
+                swap(minProd, maxProd);
+            }
+            maxProd = max(nums[i], maxProd * nums[i]);
+            minProd = min(nums[i], minProd * nums[i]);
+
+            res = max(res, maxProd);
+        }
+        return res;
+    }
+};
