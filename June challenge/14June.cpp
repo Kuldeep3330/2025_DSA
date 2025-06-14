d876e29d8c3bea6bfa3ccdc58e811a49f748ae45
Kuldeep Kumar <upsainian3330@gmail.com>
2025-06-14 21:01:19 +0530

    Symmetric Tree

---
+++ June challenge/14June.cpp
@@ -0,0 +1,34 @@
+// Symmetric Tree
+class Solution
+{
+public:
+    bool solve(struct Node *root1, struct Node *root2)
+    {
+        if (root1 == NULL && root2 == NULL)
+        {
+            return true;
+        }
+        if (root1 == NULL && root2 != NULL)
+        {
+            return false;
+        }
+        if (root1 != NULL && root2 == NULL)
+        {
+            return false;
+        }
+
+        bool lef = solve(root1->left, root2->right);
+        bool rig = solve(root1->right, root2->left);
+
+        if (root1->data == root2->data && lef && rig)
+        {
+            return true;
+        }
+        return false;
+    }
+    bool isSymmetric(Node *root)
+    {
+        // Code here
+        return solve(root, root);
+    }
+};
--- README.md
+++ README.md
@@ -319,3 +319,7 @@
 1. 121. Best Time to Buy and Sell Stock
 2. 2616. Minimize the Maximum Difference of Pairs
 3. Koko Eating Bananas(GFG)
+
+# 14 June
+
+1. Symmetric Tree
