4d4cbe7df1362de7bce8e1f4bc0677fa87db2150
Kuldeep3330 <upsainian3330@gmail.com>
2025-10-14 13:34:17 +0530

    Array in JS

--- Javascript Code/Mathematics/index.js
+++ Javascript Code/Mathematics/index.js
@@ -91,5 +91,18 @@ console.log("power", power(2,10));
 
 //Sieve of Eratosthenes
 function sieve(n){
-    
+    let primes=new Array(n+1).fill(true);
+    primes[0]=primes[1]=false;
+    for(let i=2;i*i<=n;i++){
+        if(primes[i]){
+            for(let j=i*i;j<=n;j+=i){
+                primes[j]=false;
+            }
+        }
+    }
+    let result=[];
+    for(let i=2;i<=n;i++){
+        if(primes[i]) result.push(i);
+    }
+    return result;
 }
